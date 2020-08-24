5a4aa83eb1811664e1d08ebe4f0d0c419ee6349e
cheese stick <61661631+longhike@users.noreply.github.com>
2020-08-24 13:42:16 -0400

    need to figure out passport stuff more, including why foreign ID isn't tied to current user. then, figure out generating headers in handlebars. then, add done button to each post

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -20,14 +20,23 @@ router.post("/signup", async (req, res) => {
 
 router.get('/', isAuth, (req, res) => {
     db.Posts.findAll({ order: [['createdAt', 'DESC']] })
-        .then((data) => {
-            console.log(data + " just inside the .then of router.get");
-            let hbsObject = {
-                posts: data
-            }
-            console.log(hbsObject.posts + " inside posts_controller");
-            res.render('index', hbsObject)
-        })
+    .then((data) => {
+        if (data) {
+        console.log(data + " just inside the .then of router.get");
+        let hbsObject = {
+            posts: data
+        }
+        console.log(hbsObject.posts + " inside posts_controller");
+        res.render('index', hbsObject)
+        }
+        else {
+            res.render('index')
+        }
+    })
+    .catch(() => {
+        res.sendStatus(500)
+    })
+    
 })
 
 router.get("/signup", (req, res) => {
@@ -38,13 +47,11 @@ router.get("/login", (req, res) => {
     res.render("login")
 })
 
-router.post('/api/posts', isAuth, async (req, res) => {
-    try {
-        await db.Posts.create(req.body)
-        res.redirect("/")
-    } catch (error) {
-        res.sendStatus(500)
-    }
+router.post('/api/posts', isAuth, (req, res) => {
+   db.Posts.create(req.body).then(() => {
+       res.redirect('/')
+   })
+        
 })
 
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,3 @@
 INSERT INTO Posts (username, song_title, song_artist, user_inst, user_post, createdAt, updatedAt) VALUES ("sampost", "Juicy Fruit", "Mtume", "Bass", "what a great song", current_timestamp(), current_timestamp());
+
+INSERT INTO Users (username, password, createdAt, updatedAt) VALUES ("sampost", "1234", current_timestamp(), current_timestamp());
