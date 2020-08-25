13e4c1ce82b77d383bbd0a7148eb432f32ed5373
JCZ-020 <jasoncruz_rocks@yahoo.com>
2020-08-25 18:29:36 -0400

    branch

--- public/assets/post-click.js
+++ public/assets/post-click.js
@@ -3,12 +3,13 @@ $(function () {
         e.preventDefault()
 
         let newPost = {
-            username: $('#newUser').val().trim(),
-            song_title: $('#newSong').val().trim(),
-            song_artist: $('#newArtist').val().trim(),
-            user_inst: $("#userPlays").val().trim(),
-            user_post: $('#newPost').val().trim()
-        }
+          username: $("#newUser").val().trim(),
+          song_title: $("#newSong").val().trim(),
+          song_artist: $("#newArtist").val().trim(),
+          user_inst: $("#userPlays").val().trim(),
+          user_post: $("#newPost").val().trim(),
+          media: $("#ytLink").val().trim(),
+        };
         console.log(newPost);
 
         $.ajax('/api/posts', {
