73e1de1cda95fac5d644a81da1ee41af22216f24
cheese stick <61661631+longhike@users.noreply.github.com>
2020-08-27 22:03:34 -0400

    for deploy

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const db = require("./models")
 
 const app = express()
 
-app.use(express.static("public"))
+// app.use(express.static("public"))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
 
